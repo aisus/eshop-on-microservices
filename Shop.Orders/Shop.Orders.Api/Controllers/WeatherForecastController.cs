17af51404dbf523ecae71cd5f9c8a39334f64e59
Andrey Belousov <groundhog448@gmail.com>
2020-03-02 11:36:39 +0400

    Init Orders project, add Dockerfile

--- Shop.Orders/Shop.Orders.Api/Controllers/WeatherForecastController.cs
+++ Shop.Orders/Shop.Orders.Api/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Shop.Api.Controllers
+namespace Shop.Orders.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
---
+++ Shop.Orders/Shop.Orders.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shop.Orders.Domain
+{
+    public class Class1
+    {
+    }
+}
